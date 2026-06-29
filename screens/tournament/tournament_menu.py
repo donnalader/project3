e2005794763aba2862ca3ab57c9dd0e6fb60b7fc
Donna Lader <laderd@aol.com>
2026-06-29 13:47:38 -0400

    add code to tournament menu screen

---
+++ screens/tournament/tournament_menu.py
@@ -0,0 +1,31 @@
+from commands import NoopCmd, ExitCmd
+from screens.base_screen import BaseScreen
+
+class TournamentMenu(BaseScreen):
+    """Tournament menu screen"""
+
+    def display(self):
+        print("\n=== TOURNAMENT MENU ===")
+        print("1. List tournaments")
+        print("2. Create a tournament")
+        print("3. Load a tournament")
+        print("X. Return to main menu")
+
+    def get_command(self):
+        value = self.input_string()
+
+        if value == "1":
+            return NoopCmd("tournament-list")
+
+        elif value == "2":
+            return NoopCmd("tournament-create")
+
+        elif value == "3":
+            return NoopCmd("tournament-load")
+
+        elif value.upper() == "X":
+            return NoopCmd("main-menu")
+
+        else:
+            print("Invalid choice.")
+            return NoopCmd("tournament-menu")
